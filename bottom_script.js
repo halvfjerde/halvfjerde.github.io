771e3d9ab32d4eef0654bf7d9158c8296a4cd165
halvfjerde <154803361+halvfjerde@users.noreply.github.com>
2024-02-25 13:13:35 +0000

    Update bottom_script.js

--- bottom_script.js
+++ bottom_script.js
@@ -6,5 +6,10 @@ for (i = 0; i < coll.length; i++) {
     this.classList.toggle("active");
     var content = this.nextElementSibling;  
     content.classList.toggle("inactive_content");
+    if (content.style.maxHeight === 0) {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } else {
+      content.style.maxHeight = 0;
+    };
     });
 };
