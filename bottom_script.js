68dc59a3d6a6e432ae750fa2f2fc0b7bb1d35d6c
halvfjerde <154803361+halvfjerde@users.noreply.github.com>
2024-02-25 13:17:29 +0000

    Update bottom_script.js

--- bottom_script.js
+++ bottom_script.js
@@ -5,11 +5,11 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;  
-    content.classList.toggle("inactive_content");
-    if (content.style.maxHeight === 0) {
+    if (content.classList.contains(inactive_content)) {
       content.style.maxHeight = content.scrollHeight + "px";
     } else {
       content.style.maxHeight = 0;
     };
+    content.classList.toggle("inactive_content");
     });
 };
