6fb29ca389acd54164f410f47387f47192f8a577
halvfjerde <154803361+halvfjerde@users.noreply.github.com>
2024-02-25 13:40:58 +0000

    Update bottom_script.js

--- bottom_script.js
+++ bottom_script.js
@@ -1,6 +1,8 @@
 var coll = document.getElementsByClassName("collbutton");
 var i;
 
+startUp();
+
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
