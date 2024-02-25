f635c27d8e53b3dcef494c13712e2e40728e9e4b
halvfjerde <154803361+halvfjerde@users.noreply.github.com>
2024-02-25 13:19:08 +0000

    Update bottom_script.js

--- bottom_script.js
+++ bottom_script.js
@@ -5,7 +5,7 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;  
-    if (content.classList.contains(inactive_content)) {
+    if (content.classList.contains("inactive_content")) {
       content.style.maxHeight = content.scrollHeight + "px";
     } else {
       content.style.maxHeight = 0;
