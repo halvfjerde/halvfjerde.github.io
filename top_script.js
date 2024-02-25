83e456ea2e1cc1ef13460b93f6f011ad16dafc13
halvfjerde <154803361+halvfjerde@users.noreply.github.com>
2024-02-25 13:40:37 +0000

    Update top_script.js

--- top_script.js
+++ top_script.js
@@ -1,14 +1,17 @@
 var coll = document.getElementsByClassName("collbutton");
 var j;
 
+function startUp() {
 for (j = 0; j < coll.length; j++) {
     coll[j].classList.toggle("active");
     var contentR = coll[j].nextElementSibling;  
     if (contentR.classList.contains(inactive_content)) {
-      contentR.style.maxHeight = contentR.scrollHeight + "px";
+        contentR.style.maxHeight = 0;
     } else {
-      contentR.style.maxHeight = 0;
+      contentR.style.maxHeight = contentR.scrollHeight + "px";
     };
-    contentR.classList.toggle("inactive_content");
     });
 };
+};
+
+startUp()
